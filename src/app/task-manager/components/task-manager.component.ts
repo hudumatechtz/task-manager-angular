344eaf03a0dd399bf7ddc77e6f2ad24a381845d5
Muneersahel <saidmunir73@gmail.com>
2021-02-19 09:10:07 +0300

    modify home component, change background picture

--- src/app/task-manager/components/task-manager.component.ts
+++ src/app/task-manager/components/task-manager.component.ts
@@ -4,8 +4,10 @@ import {Component} from '@angular/core';
   selector: 'app-task-manager',
   template: `
     <section class="wrapper" fxLayout="column" fxLayoutAlign="center center">
-      <div fxLayout="column" fxLayoutAlign="center center" style="padding: 10px">
-        <h3 style="margin-bottom: 20px !important; text-align: center">WELCOME TO TASK MANAGER SYSTEM</h3>
+      <div fxLayout="column" fxLayoutAlign="center center" style="padding: 10px; position: relative">
+        <div class="background" style="background: white; width: 400px !important; max-height: 400px !important; opacity: 0.7;
+         position: absolute; right: 0; left: 0; bottom: 0; top: 0"></div>
+        <h3 style="margin-bottom: 20px !important; text-align: center; z-index: 999">WELCOME TO TASK MANAGER SYSTEM</h3>
         <div fxLayout="row" style="width: 100%">
           <button mat-raised-button routerLink="/login" color="primary" style="width: 50%; margin-right: 20px">Log In</button>
           <button mat-raised-button routerLink="/register" color="primary" style="width: 50%">Register</button>
@@ -28,17 +30,15 @@ import {Component} from '@angular/core';
       width: 400px;
       border-radius: 10px;
       box-sizing: border-box;
-      background: white;
-      opacity: 0.5;
       border: 1px solid #D3D3D3;
     }
     @media (min-width: 425px) {
-      .wrapper > div {
+      .wrapper > div, .background {
         width: 400px !important;
       }
     }
     @media(max-width:768px) {
-      .wrapper > div {
+      .wrapper > div, .background {
         width: 90%;
       }
     }
